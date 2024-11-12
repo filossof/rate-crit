4ef52568608663b87d1428682abe506e3051c97e
Izik Filossof <izikfilossof@Iziks-Laptop.local>
2024-11-12 18:00:11 +0200

    added to backend

--- src/http.ts
+++ src/http.ts
@@ -1,54 +1,60 @@
+import process from "process";
+
+const port = process.env.PORT || 3000;
+
 export async function fetchPopularMovies() {
-    const response = await fetch("http://localhost:3000/popular-twenty");
-    const resData = await response.json();
+  const response = await fetch(`http://localhost:${port}/popular-twenty`);
+  const resData = await response.json();
 
-    if (!response.ok) {
-        throw new Error("Failed fetching top popular movies...");
-    }
+  if (!response.ok) {
+    throw new Error("Failed fetching top popular movies...");
+  }
 
-    return resData.results;
+  return resData.results;
 }
 
 export async function fetchTopRatedMovies() {
-    const response = await fetch("http://localhost:3000/top-rated");
-    const resData = await response.json();
+  const response = await fetch(`http://localhost:${port}/top-rated`);
+  const resData = await response.json();
 
-    if (!response.ok) {
-        throw new Error("Failed fetching top popular movies...");
-    }
+  if (!response.ok) {
+    throw new Error("Failed fetching top popular movies...");
+  }
 
-    return resData.results;
+  return resData.results;
 }
 
 export async function fetchNowPlayingMovies() {
-    const response = await fetch("http://localhost:3000/now-playing");
-    const resData = await response.json();
+  const response = await fetch(`http://localhost:${port}/now-playing`);
+  const resData = await response.json();
 
-    if (!response.ok) {
-        throw new Error("Failed fetching now-playing movies...");
-    }
+  if (!response.ok) {
+    throw new Error("Failed fetching now-playing movies...");
+  }
 
-    return resData.results;
+  return resData.results;
 }
 
 export async function fetchMoviesByGenre(genreId: number) {
-    const response = await fetch(`http://localhost:3000/genre?id=${genreId}`);
-    const resData = await response.json();
+  const response = await fetch(`http://localhost:${port}/genre?id=${genreId}`);
+  const resData = await response.json();
 
-    if (!response.ok) {
-        throw new Error("Failed fetching movies in <genre> genre");
-    }
+  if (!response.ok) {
+    throw new Error("Failed fetching movies in <genre> genre");
+  }
 
-    return resData.results;
+  return resData.results;
 }
 
 export async function fetchMoviesByQuery(searchQuery: string) {
-    const response = await fetch(`http://localhost:3000/search?searchQuery=${searchQuery}`);
-    const resData = await response.json();
+  const response = await fetch(
+    `http://localhost:${port}/search?searchQuery=${searchQuery}`
+  );
+  const resData = await response.json();
 
-    if (!response.ok) {
-        throw new Error("Failed fetching movies in <genre> genre");
-    }
+  if (!response.ok) {
+    throw new Error("Failed fetching movies in <genre> genre");
+  }
 
-    return resData.results;
+  return resData.results;
 }
